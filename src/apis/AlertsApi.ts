7a891c6b6fb36a573d2fda569eb69669461b537e
kevin <kevindabot@emergentmethods.ai>
2025-01-29 20:36:18 +0000

    release(0.2.37): Release typescript-fetch SDK

--- src/apis/AlertsApi.ts
+++ src/apis/AlertsApi.ts
@@ -51,6 +51,7 @@ export interface GetAlertRequest {
 
 export interface GetAlertLogsRequest {
     alertId: string;
+    userId?: string;
     page?: number;
     perPage?: number;
     all?: boolean;
@@ -198,6 +199,10 @@ export class AlertsApi extends runtime.BaseAPI {
         }
 
         const queryParameters: any = {};
+
+        if (requestParameters['userId'] != null) {
+            queryParameters['user_id'] = requestParameters['userId'];
+        }
 
         if (requestParameters['page'] != null) {
             queryParameters['page'] = requestParameters['page'];
